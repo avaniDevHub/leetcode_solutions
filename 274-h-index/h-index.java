0da6b23836305ebd7bccb72388463a90585337ad
avaniDevHub <malviyaavani31@gmail.com>
2026-01-03 12:34:56 +0530

    Time: 5 ms (65.79%) | Memory: 43.4 MB (55.14%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int ans=0;
+        Arrays.sort(citations);
+        for(int i=citations.length-1;i>=0;i--)
+        {
+            if(ans<citations[i])
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
