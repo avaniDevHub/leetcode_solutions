df1e7ffc7f196073fcb26dbcceb7063e34af8b02
avaniDevHub <malviyaavani31@gmail.com>
2026-07-17 14:26:57 +0530

    Time: 14 ms (90.29%) | Memory: 43.6 MB (59.64%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,34 +1,33 @@
 class Solution {
     public String longestPalindrome(String s) {
-        for(int length=s.length();length>0; length--)
-        {
-            for(int start=0;start<=s.length()-length;start++)
-            {
-                if(check(start,start+length,s))
-                {
-                    return s.substring(start,start+length);
-                }
+        if (s == null || s.length() < 1) return "";
+        
+        int start = 0, end = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            // Case 1: Palindrome has an odd length (center is a single character)
+            int len1 = expandAroundCenter(s, i, i);
+            // Case 2: Palindrome has an even length (center is between two characters)
+            int len2 = expandAroundCenter(s, i, i + 1);
+            
+            int len = Math.max(len1, len2);
+            
+            // If we found a longer palindrome, update our tracking indices
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
             }
         }
-        return "";
+        
+        return s.substring(start, end + 1);
     }
-
-    private boolean check(int i, int j, String s)
-    {
-        int left=i;
-        int right= j-1;
-
-        while(left<right)
-        {
-            if(s.charAt(left)!= s.charAt(right))
-            {
-                return false;
-            }
-
-            left++;
-            right--;
-
+    
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
         }
-        return true;
+        // Return the length of the palindrome found
+        return right - left - 1;
     }
 }
