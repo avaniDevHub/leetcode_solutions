4103646aabd1c236d5043ef689ed1474ba476d5d
avaniDevHub <malviyaavani31@gmail.com>
2026-07-22 21:41:13 +0530

    Time: 3 ms (99.09%) | Memory: 81.2 MB (61.75%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.java
+++ 2478-longest-nice-subarray/longest-nice-subarray.java
@@ -1,19 +1,22 @@
 class Solution {
     public int longestNiceSubarray(int[] nums) {
-        int maxLength = 1;
-        int n = nums.length;
+        int left = 0;
+        int usedBits = 0;
+        int maxLength = 0;
 
-        for (int i = 0; i < n; i++) {
-            int usedBits = 0;
-            for (int j = i; j < n; j++) {
-                // If there is a bit collision, this subarray is invalid
-                if ((usedBits & nums[j]) != 0) {
-                    break;
-                }
-                // Add current number's bits to our mask
-                usedBits |= nums[j];
-                maxLength = Math.max(maxLength, j - i + 1);
+        for (int right = 0; right < nums.length; right++) {
+            // If the incoming number shares any bit with the current window,
+            // shrink the window from the left until the conflict is cleared.
+            while ((usedBits & nums[right]) != 0) {
+                usedBits ^= nums[left]; // Unset bits of nums[left]
+                left++;
             }
+
+            // Include current number's bits in the active window
+            usedBits |= nums[right];
+
+            // Record the window size
+            maxLength = Math.max(maxLength, right - left + 1);
         }
 
         return maxLength;
