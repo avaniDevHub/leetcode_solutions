3c3a51097b4df3177424588c3701839bff0dfedf
avaniDevHub <malviyaavani31@gmail.com>
2026-08-11 14:52:45 +0530

    Time: 1 ms (99.59%) | Memory: 44.2 MB (16.49%) - LeetSync

---
+++ 841-shortest-distance-to-a-character/shortest-distance-to-a-character.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] shortestToChar(String s, char c) {
+        int n=s.length();
+        int[] answer= new int[s.length()];
+// Pass 1: Left to Right
+        int prev=-n;
+        for(int i=0;i<n;i++)
+        {
+            if(s.charAt(i) == c)
+            {
+                prev=i;
+            }
+            answer[i]= i-prev;
+        }
+// Pass 1: Left to Right
+        prev=2*n;
+        for(int i=n-1;i>=0; i--)
+        {
+            if(s.charAt(i)==c)
+            {
+                prev=i;
+            }
+            answer[i]=Math.min(answer[i], prev-i);
+        }
+        return answer;
+
+
+    }
+}
